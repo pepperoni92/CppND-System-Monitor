8d321331d5582edf9fdfebc5d84780553a205f64
Joey L <jlapegna@gmail.com>
2021-02-22 23:57:48 +0000

    Showing operating system name and % character on progress bars

--- src/system.cpp
+++ src/system.cpp
@@ -26,8 +26,8 @@ std::string System::Kernel() { return string(); }
 // Return the system's memory utilization
 float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// Return the operating system name
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
