f624a33a04828ed6a3fe9f6ce047d414a5f48e5b
joey <jlapegna@gmail.com>
2021-02-22 00:00:09 +0000

    Added helper function tonconvert seconds to HH:MM:SS string

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,34 @@
+#include <sstream>
 #include <string>
 
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds)
+{
+    // divide seconds by 60 to get minutes
+    long m = seconds / 60;
+
+    // seconds modulus of 60 to get remaining seconds
+    long s = seconds % 60;
+
+    // divide hours by 24 to get remaining days
+    long h = m / 60;
+
+    // hours modulus of 24 to get remaining minutes
+    m = m % 60;
+    
+    std::stringstream ss; 
+
+    if (h < 10) ss << "0";
+    ss << std::to_string(h)  << ":";
+    
+    if (m < 10) ss << "0";
+    ss << std::to_string(m) << ":";
+
+    if (s < 10) ss << "0";
+    ss << std::to_string(s);
+    
+    return ss.str();
+}
